9c368435d5e2ae7ca553709aaa90971c5ceee383
Niels Søholm <nm@9la.dk>
2014-04-23 23:07:13 +0200

    Adjusted merge code to merge all relevant .csv files in the directory. Also added even more specific labels

--- mortality/merge-attributes.py
+++ mortality/merge-attributes.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 def joinAttribute(headers, countries, f_in):
 	for line in f_in:
@@ -19,29 +20,31 @@ def joinAttribute(headers, countries, f_in):
 			country[year].append(attr)
 
 
-files = [
-"CICDALLC-NBPOPUPC",
-"CICDALLC-NBMALEPH",
-"CICDALLC-NBFEMEPF"]
+files = []
+root = "../parser/"
+for f in os.listdir(root):
+	path = root + f
+	if(re.search("(Health_stats\.xml\-[A-Z0-9]+\-[A-Z0-9]+\.csv)", path) != None):
+		if(path not in files):
+			files.append(path)
 
 # Read headers
-path = "../parser/Health_stats.xml-{0}.csv".format(files[0])
+path = files[0]
 f_in = open(path, "r")
 headers = f_in.next().rstrip("\n").split(',')
 f_in.close()
 
 # Read and join data
 countries = dict()
-for fl in files:
-	path = "../parser/Health_stats.xml-{0}.csv".format(fl)
+for path in files:
 	f_in = open(path, "r")
 	f_in.next() # Skip header
 	joinAttribute(headers, countries, f_in)
 f_in.close()
 
 # Preview
-for ccode in countries.keys():
-	print("{0}: {1}".format(ccode, countries[ccode]))
+#for ccode in countries.keys():
+#	print("{0}: {1}".format(ccode, countries[ccode]))
 
 # Produce joined csv header
 f_in = open("../parser/HEALTH_STAT_LABELS.csv", "r")
@@ -50,12 +53,21 @@ for line in f_in:
 	match_c = re.search("([A-Z]+),\"?(.+)\"?", line)
 	labelmap[match_c.group(1)] = match_c.group(2)
 
-headerstr = "\"Country\",\"Year\","
+f_in2 = open("../parser/HEALTH_STAT_LABELS_VAR.csv", "r")
+varmap = dict()
+for line in f_in2:
+	match_c = re.search("\"([A-Z0-9]+)\",\"(.+)\"", line)
+	varmap[match_c.group(1)] = match_c.group(2)
+
+
+headerstr = "\"Country\";\"Year\";"
 for fl in files:
-	label = fl.split("-")[1]
-	detailed = labelmap[label]
-	headerstr += "\"{0}\",".format(detailed)
-headerstr = headerstr.rstrip(",")
+	match_c = re.search("Health_stats\.xml\-([A-Z0-9]+)\-([A-Z0-9]+)\.csv", fl)
+	varlabel = match_c.group(1)
+	label = match_c.group(2)
+	detailed = "{0} [{1}]".format(varmap[varlabel], labelmap[label])
+	headerstr += "\"{0}\";".format(detailed)
+headerstr = headerstr.rstrip(";")
 headerstr += "\n"
 
 # Produce joined csv
@@ -63,10 +75,10 @@ csv = ""
 for ccode in countries.keys():
 	country = countries[ccode]
 	for year in country.keys():
-		csv += "{0},{1},".format(ccode, year)
+		csv += "{0};{1};".format(ccode, year)
 		for val in country[year]:
-			csv += "{0},".format(val)
-		csv = csv.rstrip(",")
+			csv += "{0};".format(val)
+		csv = csv.rstrip(";")
 		csv += "\n"
 
 # Output to csv
