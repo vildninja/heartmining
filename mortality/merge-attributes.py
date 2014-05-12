d74864080b9c41db4ec8e9e1a33fdd49425a7197
Niels Søholm <nm@9la.dk>
2014-05-12 11:55:25 +0200

    Adjusted mortality merge script to work with the new interpolated set. Added functionality to insert empty string at missing values and trash countries with more than 120 missing features

--- mortality/merge-attributes.py
+++ mortality/merge-attributes.py
@@ -1,30 +1,43 @@
 import re
 import os
 
-def joinAttribute(headers, countries, f_in):
+# Produce joined csv header
+f_in = open("../parser/HEALTH_STAT_LABELS_COUNTRY.csv", "r")
+ccodes = []
+for line in f_in:
+	ccodes.append(line.rstrip("\n"))
+
+
+def joinAttribute(headers, countries, misscount, f_in):
+	cseen = dict()
+	for ccode in ccodes:
+		cseen[ccode] = False
+
 	for line in f_in:
 		line = line.rstrip("\n")
 		attrs = line.split(',')
 		name = attrs[0]
+		country = countries[name]
 
-		country = dict()
-		if(name in countries.keys()):
-			country = countries[name]
-		else:
-			countries[name] = country
+		cseen[name] = True
 
 		for i,attr in enumerate(attrs[1:]):
 			year = headers[i+1]
-			if(year not in country.keys()):
-				country[year] = []
 			country[year].append(attr)
+
+	for ccode in cseen.keys():
+		if(not cseen[ccode]):
+			misscount[ccode] = misscount[ccode] + 1
+			country = countries[ccode]
+			for year in country.keys():
+				country[year].append("")
 
 
 files = []
-root = "../parser/"
+root = "../parser/interpolated/"
 for f in os.listdir(root):
 	path = root + f
-	if(re.search("(Health_stats\.xml\-[A-Z0-9]+\-[A-Z0-9]+\.csv)", path) != None):
+	if(re.search("Health_stats\.xml\-([A-Z0-9]+)\-([A-Z0-9]+)\.csv", path) != None):
 		if(path not in files):
 			files.append(path)
 
@@ -34,13 +47,30 @@ f_in = open(path, "r")
 headers = f_in.next().rstrip("\n").split(',')
 f_in.close()
 
-# Read and join data
+# Prepare country dictionary to store data
 countries = dict()
+misscount = dict()
+for ccode in ccodes:
+	country = dict()
+	for year in range(2000,2012):
+		country[str(year)] = []
+	countries[ccode] = country
+	misscount[ccode] = 0
+
+# Read and join data
 for path in files:
 	f_in = open(path, "r")
 	f_in.next() # Skip header
-	joinAttribute(headers, countries, f_in)
+	joinAttribute(headers, countries, misscount, f_in)
 f_in.close()
+
+# Remove countries with too high miss count
+threshold = 120
+for country in misscount.keys():
+	miss = misscount[country]
+	if(miss > threshold):
+		print("'{0}' is missing too many features ({1}/547)".format(country, miss))
+		countries.pop(country, None)
 
 # Preview
 #for ccode in countries.keys():
@@ -60,13 +90,14 @@ for line in f_in2:
 	varmap[match_c.group(1)] = match_c.group(2)
 
 
-headerstr = "\"Country\";\"Year\";"
+headerstr = "Country;Year;"
 for fl in files:
 	match_c = re.search("Health_stats\.xml\-([A-Z0-9]+)\-([A-Z0-9]+)\.csv", fl)
 	varlabel = match_c.group(1)
 	label = match_c.group(2)
 	detailed = "{0} [{1}]".format(varmap[varlabel], labelmap[label])
-	headerstr += "\"{0}\";".format(detailed)
+	headerstr += "{0};".format(detailed)
+	#print detailed
 headerstr = headerstr.rstrip(";")
 headerstr += "\n"
 
@@ -82,6 +113,6 @@ for ccode in countries.keys():
 		csv += "\n"
 
 # Output to csv
-f_out = open("Health_stats.xml-merged.csv", "w")
+f_out = open("Health_stats.xml-interpolated-merged.csv", "w")
 f_out.write(headerstr)
 f_out.write(csv)
